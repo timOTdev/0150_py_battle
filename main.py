f8d1a9d50cb910e8ee4963bdc37e5eb568647ed7
Timothy Hoang <timh1203@gmail.com>
2019-04-16 16:37:27 -0500

    Add enemy instantiation and attack scenarios

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ magic = [{"name": "Fire", "cost": 10, "dmg": 60},
          {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "An ENEMY ATTACKS!" + bcolors.ENDC)
+
+while running:
+    print("=====================")
+    player.choose_action()
+    choice = input("Choose action:")
+    index = int(choice) - 1
+    
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("You attacked for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+
+    enemy_choice = 1
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp())
